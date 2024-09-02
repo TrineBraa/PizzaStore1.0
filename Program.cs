f3864828476af0e3ca68572cf2439d558dab9e61
trinebra <157688079+trinebra@users.noreply.github.com>
2024-09-02 13:25:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("Items"));
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "PizzaStore API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI( c => 
+    {
+        c.SwaggerEndpoint("/Swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
+
+
+app.MapGet("/pizzas", async (PizzaDB db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizza", async (PizzaDB db, Pizza pizza) => 
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+app.MapGet("/pizza/{id}", async (PizzaDB db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPut("/pizza/{id}", async (PizzaDB db, Pizza updatepizza, int id) => 
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+  if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/pizza/{id}", async (PizzaDB db, int id) => 
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();       
+});
+
+
+app.Run();
